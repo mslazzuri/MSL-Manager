282b5e29abaf53939208980e1f13830713db90e8
matheussecco <mslazzuri@mavs.coloradomesa.edu>
2025-05-18 15:03:32 -0600

    Connected to Firebase backend

--- msl_manager/lib/main.dart
+++ msl_manager/lib/main.dart
@@ -1,6 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:msl_manager/firebase_options.dart';
 
-void main() {
+Future <void> main() async {
+  
+  // Initialize Firebase
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  
+
+  // Run the app
   runApp(const MainApp());
 }
 
