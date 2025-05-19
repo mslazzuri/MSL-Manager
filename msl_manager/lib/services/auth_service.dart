074f6854c597950dd531bf982a15f6b80674e7e2
matheussecco <mslazzuri@mavs.coloradomesa.edu>
2025-05-18 23:18:53 -0600

    .

--- msl_manager/lib/services/auth_service.dart
+++ msl_manager/lib/services/auth_service.dart
@@ -1,6 +1,5 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:firebase_core/firebase_core.dart';
 
 class AuthService {
   final FirebaseAuth _auth = FirebaseAuth.instance;
